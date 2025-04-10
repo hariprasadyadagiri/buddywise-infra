bad739c56464f5b156f293aef172a421a494a8cf
Hari Prasad <yadagirihari@gmail.com>
2025-04-10 14:28:08 +0200

    updated infra with terraform yml

--- infra/provider.tf
+++ infra/provider.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
   features {}
 
   use_oidc              = true
-  client_id             = var.AZURE_CLIENT_ID
-  subscription_id       = var.AZURE_SUBSCRIPTION_ID
-  tenant_id             = var.AZURE_TENANT_ID
+  client_id             = secrets.AZURE_CLIENT_ID
+  subscription_id       = secrets.AZURE_SUBSCRIPTION_ID
+  tenant_id             = secrets.AZURE_TENANT_ID
 }
