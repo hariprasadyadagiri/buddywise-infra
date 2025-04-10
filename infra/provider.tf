ef312632d349822cfa046356bbf04efde031a13f
Hari Prasad <yadagirihari@gmail.com>
2025-04-10 11:44:54 +0200

    updated infra with terraform yml

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,13 +1,10 @@
+# 
+
 provider "azurerm" {
   features {}
-  subscription_id = "cf13ac48-763a-4dd6-b402-af89cef1a1ed"
-}
 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "buddy-wise"
-    storage_account_name = "saakstfstateprod"
-    container_name       = "tfstate-prod"
-    key                  = "terraform.tfstate"
-  }
+  use_oidc              = true
+  client_id             = var.AZURE_CLIENT_ID
+  subscription_id       = var.AZURE_SUBSCRIPTION_ID
+  tenant_id             = var.AZURE_TENANT_ID
 }
