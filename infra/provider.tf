8671a114dd8230e5c8955d8619e9080426145b85
Hari Prasad <yadagirihari@gmail.com>
2025-04-11 07:55:35 +0200

    Terraform Update

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,10 +1,5 @@
 provider "azurerm" {
   features {}
-
-  client_id        = var.AZURE_CLIENT_ID
-  tenant_id        = var.AZURE_TENANT_ID
-  subscription_id  = var.AZURE_SUBSCRIPTION_ID
-  client_secret    = var.AZURE_CLIENT_SECRET
 }
 terraform {
   backend "azurerm" {
@@ -12,6 +7,11 @@ terraform {
     storage_account_name = "saakstfstateprod"
     container_name       = "tfstate-prod"
     key                  = "terraform.tfstate"
+
+    client_id        = var.AZURE_CLIENT_ID
+    tenant_id        = var.AZURE_TENANT_ID
+    subscription_id  = var.AZURE_SUBSCRIPTION_ID
+    client_secret    = var.AZURE_CLIENT_SECRET
   }
 }
 
