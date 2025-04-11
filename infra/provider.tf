ad0e959b32a2de567c7ce2e2048db4b563a7aa13
Hari Prasad <yadagirihari@gmail.com>
2025-04-11 08:05:37 +0200

    Terraform Update

--- infra/provider.tf
+++ infra/provider.tf
@@ -11,12 +11,9 @@ terraform {
     client_id        = "825b698e-807f-491b-91fe-1cddc869cef1"
     tenant_id        = "2ac9248b-50f0-4b2d-a4a6-5e6a82376bdb"
     subscription_id  = "cf13ac48-763a-4dd6-b402-af89cef1a1ed"
-    client_secret    = var.AZURE_CLIENT_SECRET
+    # client_secret    = var.AZURE_CLIENT_SECRET
   }
 }
 
 
 
-variable "AZURE_CLIENT_SECRET" {
-  type = string
-}
