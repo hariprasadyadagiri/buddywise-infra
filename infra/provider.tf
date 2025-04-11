f8efa462f39a19793f4b37059fe6a50004d062d4
Hari Prasad <yadagirihari@gmail.com>
2025-04-11 07:49:47 +0200

    Terraform Update

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,7 +1,6 @@
 provider "azurerm" {
   features {}
 
-  use_oidc         = true
   client_id        = var.AZURE_CLIENT_ID
   tenant_id        = var.AZURE_TENANT_ID
   subscription_id  = var.AZURE_SUBSCRIPTION_ID
