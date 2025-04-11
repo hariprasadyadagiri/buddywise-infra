edee3aec91bd0f48a95ce571ad688bc3c2eb5400
Hari Prasad <yadagirihari@gmail.com>
2025-04-11 06:21:31 +0200

    Terraform Update

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,5 +5,16 @@ provider "azurerm" {
   client_id        = var.AZURE_CLIENT_ID
   tenant_id        = var.AZURE_TENANT_ID
   subscription_id  = var.AZURE_SUBSCRIPTION_ID
-
 }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "buddy-wise"
+    storage_account_name = "saakstfstateprod"
+    container_name       = "tfstate-prod"
+    key                  = "terraform.tfstate"
+  }
+}
+
+
+
+
