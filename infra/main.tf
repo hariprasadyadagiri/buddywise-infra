719fc2a08e49a1f55bf494e6a0f826a0385358fd
Hari Prasad <yadagirihari@gmail.com>
2025-04-11 08:08:07 +0200

    Terraform Update

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,19 @@
+provider "azurerm" {
+  features {}
+}
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "buddy-wise"
+    storage_account_name = "saakstfstateprod"
+    container_name       = "tfstate-prod"
+    key                  = "terraform.tfstate"
 
+    # client_id        = "825b698e-807f-491b-91fe-1cddc869cef1"
+    # tenant_id        = "2ac9248b-50f0-4b2d-a4a6-5e6a82376bdb"
+    # subscription_id  = "cf13ac48-763a-4dd6-b402-af89cef1a1ed"
+    # client_secret    = var.AZURE_CLIENT_SECRET
+  }
+}
 resource "azurerm_resource_group" "rg" {
   name     = "buddy-wise"
   location = "westus"  
