4630a9c0e0af383b32553442de9bff8f57a0ca6d
Hari Prasad <yadagirihari@gmail.com>
2025-04-11 08:04:07 +0200

    Terraform Update

--- infra/provider.tf
+++ infra/provider.tf
@@ -8,24 +8,14 @@ terraform {
     container_name       = "tfstate-prod"
     key                  = "terraform.tfstate"
 
-    client_id        = var.AZURE_CLIENT_ID
-    tenant_id        = var.AZURE_TENANT_ID
-    subscription_id  = var.AZURE_SUBSCRIPTION_ID
+    client_id        = "825b698e-807f-491b-91fe-1cddc869cef1"
+    tenant_id        = "2ac9248b-50f0-4b2d-a4a6-5e6a82376bdb"
+    subscription_id  = "cf13ac48-763a-4dd6-b402-af89cef1a1ed"
     client_secret    = var.AZURE_CLIENT_SECRET
   }
 }
 
-variable "AZURE_CLIENT_ID" {
-  type = string 
-}
 
-variable "AZURE_TENANT_ID" {
-  type = string    
-}
-
-variable "AZURE_SUBSCRIPTION_ID" {
-  type = string    
-}
 
 variable "AZURE_CLIENT_SECRET" {
   type = string
